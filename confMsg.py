819739174029d78029ebd6899927cd34101ef20d
Daniel Hampl <danhampl31415@gmail.com>
2019-05-12 21:06:55 +0200

    travic cof magic

--- confMsg.py
+++
@@ -1,4 +0,0 @@
-FIRST_ENDING = '\nSend n for next page.'
-DEFAULT_ENDING = '\nSend n for next page or p for previous.'
-LAST_ENDING = '\nLast page, send p for previous page.'
-ERROR = "Wrong input."
