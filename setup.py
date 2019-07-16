e5f9992d16d74e57b5aeecff9abc50641ac7aa6c
Daniel Hampl <danhampl31415@gmail.com>
2019-07-16 23:15:23 +0200

    cant find discord.py-rw fix

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'discord.py-rw',
+        # 'discord.py-rw',
+        'discord.py',
         'requests',
     ],
     setup_requires=[
